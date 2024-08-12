5e8cf1f2adf40b1992d4950a39a07be0026783bc
Itumeleng-Koalane <koalane567@gmail.com>
2024-08-12 21:24:19 +0200

    UI Functionality not yet working

---
+++ src/app/pages/register-screen/register-screen.component.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms'; 
+
+@Component({
+  selector: 'app-register-screen',
+  standalone: true,
+  imports: [],
+  templateUrl: './register-screen.component.html',
+  styleUrl: './register-screen.component.css',
+})
+
+export class RegisterScreenComponent {
+
+}
+
+
